f8aae2a1b94a2ddd61a0d45de0b86a1754e47865
pdlp271 <109448588+pdlp271@users.noreply.github.com>
2025-07-03 10:52:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -80,7 +80,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")
+URL = environ.get("URL", "https://revolutionary-anna-hprajbots-99faf928.koyeb.app/")
 
 
 # Don't Remove Credit Tg - @VJ_Botz
